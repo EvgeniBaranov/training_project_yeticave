0336690b5b5f7bb9aaf5ef376fbbaf3d1d454294
EvgeniBaranov <109065399+EvgeniBaranov@users.noreply.github.com>
2022-12-03 08:55:39 +0500

    Delete 404.php

--- 404.php
+++
@@ -1,6 +0,0 @@
-<main class="container">
-    <section class="lot-item container">
-            <h2>404 Страница не найдена</h2>
-            <p>Данной страницы не существует на сайте.</p>
-        </section>
-</main>
